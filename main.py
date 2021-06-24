de43ac9f8cdc7b770e2cd6f0e973e096c8dccae4
B-Pasteelnick <bpasteelnick@gmail.com>
2021-06-23 22:42:11 -0400

    tracking channels for remember

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 client = discord.Client()
 token = os.getenv("TOKEN")
 client.state = "ready"
+client.activeChannels = []
 
 @client.event
 async def on_ready():
@@ -61,14 +62,16 @@ async def on_message(message):
         bars = '\n**-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------**\n'
         first = 'You awaken alone in a shadowed room. A flickering, sourceless, flame, mere inches away, makes you aware of the drifting grey shape of this place. You cannot see the dark edges that skulk from the light... or perhaps you cannot remember. Memory begins to flood back, though jagged holes and shifting faces keep you from recalling clear. You remember *what you are*. You do not remember **why you are here**. You remember *who you were*. You do not remember **your name**. You remember *what you can do*. You do not remember **what you must do**.\n\nSomething... powerful... glints in the fire. Your hand, outstretched, runs and billows like smoke. You cannot reach it, not yet. When your memories - those that have not deserted you - are solid in your mind, then your body will be ready to bear the heat of the crucible!'
 
+        if message.channel in client.activeChannels:
+          await message.channel.send("You are already remembering.")
+          return
+
         m = await message.channel.send(bars + first + bars)
         await asyncio.sleep(10)
-
-        while (client.state != "ready"):
-          await asyncio.sleep(10)
+        
         bold = False;
         italics = False;
-        client.state = "busy"
+        client.activeChannels.append(message.channel)
 
         for i in first:
           while first.startswith ('\n'):
@@ -87,10 +90,10 @@ async def on_message(message):
             italics = False
             first = first[0] + first[2:]
           
-          await asyncio.sleep(.5)
+          await asyncio.sleep(1)
           if (first == '' or first[1:] == ''):
             await m.edit(content="You seem to have forgotten...")
-            client.state = "ready"
+            client.activeChannels.remove(message.channel)
             return
           first = first[1:]
           if first.startswith(" "): first = first[1:]
@@ -98,7 +101,8 @@ async def on_message(message):
           if bold and not first.startswith("**"): await m.edit(content= bars + "**" + first + bars)
           elif italics and not first.startswith("*"): await m.edit(content= bars + "*" + first + bars)
           else: await m.edit(content=bars + first + bars)
-        client.state = "ready"
+          client.activeChannels.remove(message.channel)
+
 
       else:
         await message.channel.send(oMess[1:])
