42ef8f318268a22d981483b80b2ce6509555a780
B-Pasteelnick <bpasteelnick@gmail.com>
2021-06-28 20:11:51 -0400

    announcements v4

--- main.py
+++ main.py
@@ -46,15 +46,15 @@ async def on_message(message):
       print(message.content)
 
       checkGuide = discord.utils.get(message.guild.roles, name='Guides')
-      if checkGuide in message.author.roles and message.startswith('announceall'):
+      if checkGuide in message.author.roles and message.content.startswith('announceall'):
         for i in allPCs:
           await client.get_channel(i).send(message[11:])
         return
-      elif checkGuide in message.author.roles and message.startswith('announceaside'):
+      elif checkGuide in message.author.roles and message.content.startswith('announceaside'):
         for i in ASideChannels:
           await client.get_channel(i).send(message[13:])
         return
-      elif checkGuide in message.author.roles and message.startswith('announcebside'):
+      elif checkGuide in message.author.roles and message.content.startswith('announcebside'):
         for i in BSideChannels:
           await client.get_channel(i).send(message[13:])
         return
