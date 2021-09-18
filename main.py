a0a722615e94d45f11d4b9a95e373e50fa695cc2
Ben <BPasteelnick@gmail.com>
2021-09-17 23:30:00 -0400

    teachers

--- main.py
+++ main.py
@@ -805,6 +805,14 @@ async def on_message(message):
         m = await message.channel.send('We always thought it was ironic, considering what happened.')
         await self_edit(m)
 
+      elif message.content == ('mr.young'):
+        m = await message.channel.send('Wished he was nicer.')
+        await self_edit(m)
+
+      elif message.content == ('ms.harriet'):
+        m = await message.channel.send('What an asshole.')
+        await self_edit(m)
+
       elif message.content == ('esarebrokenwiththepowerd'):
         await message.channel.send(file=discord.File('&esarebrokenwiththePowerD.png'))
 
@@ -814,7 +822,7 @@ async def on_message(message):
       elif message.content == ('card46361a71632e9810d8422c00c49029e9'):
         await message.channel.send(file=discord.File('The GD\'s Lab.png'))
 
-      #elif message.content == ('14e49010c0cf571e3133a585dd833789') or message.content == ('0e3964e70f6aca22f1edc573a01882eb')
+      #elif message.content == ('14e49010c0cf571e3133a585dd833789') or message.content == ('0e3964e70f6aca22f1edc573a01882eb') or message.content == ('03e423335d50461e14f5c71bb089e861') or message.content == ('157546964e5aa203d49b80c828aa53db') or message.content == ('5e06edb4ed226b1711b4acfb7a3aefa3')
 
 
       elif message.content == ('remember'):
